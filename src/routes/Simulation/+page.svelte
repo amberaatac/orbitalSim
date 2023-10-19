11c9133afd0cb4d3290128e09e964acfdabccd1b
amberaatac <amber.atac@gmail.com>
2023-10-18 18:00:15 -0700

    init

---
+++ src/routes/Simulation/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+const GravConstant =  6.67*10**-11 // Big G
+let PlanetMass, SatMass, Radius
+
+function calcVelocity() {
+    return Math.sqrt((2*calcKE())/(SatMass))
+}
+
+function calcKE() {
+    return (GravConstant*PlanetMass*SatMass)/(2*Radius);
+}
+
+function calcPE() {
+    return -(GravConstant*PlanetMass*SatMass)/(Radius);
+}
+
+function calcTE() {
+    return calcKE() + calcPE(); 
+}
+</script>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
