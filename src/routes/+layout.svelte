b00ebb7bdbf67b9e997a3ff28039e70d8a3798e8
amberaatac <amber.atac@gmail.com>
2023-10-25 02:19:52 -0700

    added img

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,13 @@
+<script>
+    import "../app.css"
+</script>
+
+
 <slot/> 
+
+<style>
+    :global(body) {
+        background-image: url("../lib/bg.png");
+        background-size: cover;
+    }
+</style>
