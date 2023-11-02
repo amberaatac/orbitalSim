6ab4f1ccb94ab8c1ce6e6b58a67d0afa7024a773
amberaatac <62476412+amberaatac@users.noreply.github.com>
2023-11-01 19:17:28 -0700

    Delete .vercel directory

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.17154ffe.js","_app/immutable/chunks/scheduler.b95eede2.js","_app/immutable/chunks/index.e270efd4.js"];
-export const stylesheets = ["_app/immutable/assets/2.96d9ca1f.css","_app/immutable/assets/app.7614952e.css"];
-export const fonts = [];
